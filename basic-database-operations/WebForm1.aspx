4cc82dc39a98b507b400b797b1e045235337d661
RohitBari <160343594+Rohitbari27@users.noreply.github.com>
2024-07-24 16:30:25 +0530

    Add files via upload

---
+++ basic-database-operations/WebForm1.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="basic_database_operations.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+         <table>
+            <tr>
+                <td>First name</td>
+                <td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Last name</td>
+                <td><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></td>
+            </tr>
+
+            <tr>
+                <td>Number</td>
+                <td><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="BtnSubmit" runat="server" Text="Insert" OnClick="BtnSubmit_Click" />
+                   
+                    <asp:Button ID="BtnView" runat="server" Text="View" OnClick="btnView_Click"/>
+
+                    <asp:Button ID="btnDeleteAll" runat="server" Text="Delete All" OnClick="btnDeleteAll_Click" />
+
+                    <asp:Button ID="btnDeleteRow" runat="server" Text="Delete Row" OnClick="btnDeleteRow_Click" />
+
+                </td>
+            </tr>
+            </table>
+
+            <h2>GridView</h2>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="True"></asp:GridView>
+
+            <h2>DataList</h2>
+            <asp:DataList ID="DataList1" runat="server">
+                <ItemTemplate>
+                    <div>
+
+                        <b>FirstName:</b> <%# Eval("FirstName") %> <br />
+                        <b>LastName:</b> <%# Eval("LastName") %> <br />
+                        <b>Number:</b> <%# Eval("Number") %> <br />
+                    </div>
+                </ItemTemplate>
+            </asp:DataList>
+
+            <h2>ListView</h2>
+            <asp:ListView ID="ListView1" runat="server">
+                <ItemTemplate>
+                    <div>
+                      
+                        <b>FirstName:</b> <%# Eval("FirstName") %> <br />
+                        <b>LastName:</b> <%# Eval("LastName") %> <br />
+                        <b>Number:</b> <%# Eval("Number") %> <br />
+                    </div>
+                </ItemTemplate>
+            </asp:ListView>
+
+            <h2>Repeater</h2>
+            <asp:Repeater ID="Repeater1" runat="server">
+                <ItemTemplate>
+                    <div>
+                      
+                        <b>FirstName:</b> <%# Eval("FirstName") %> <br />
+                        <b>LastName:</b> <%# Eval("LastName") %> <br />
+                        <b>Number:</b> <%# Eval("Number") %> <br />
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ basic-database-operations/WebForm1.aspx.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace basic_database_operations
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string firstName = TextBox1.Text;
+                string lastName = TextBox2.Text;
+                int number;
+                if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || !int.TryParse(TextBox3.Text, out number))
+                {
+                    Response.Write("<script>alert('Please enter valid values for all fields');</script>");
+                    return;
+                }
+                else
+                {
+                    string cs = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=db; Integrated Security=True;";
+                    using (SqlConnection con = new SqlConnection(cs))
+                    {
+                        con.Open();
+                        string q = "INSERT INTO demo (FirstName, LastName, number) VALUES (@firstName, @lastName, @number)";
+                        using (SqlCommand cmd = new SqlCommand(q, con))
+                        {
+                            cmd.Parameters.AddWithValue("@firstName", firstName);
+                            cmd.Parameters.AddWithValue("@lastName", lastName);
+                            cmd.Parameters.AddWithValue("@number", number);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                Response.Write($"<script>alert('Inserted successfully');</script>");
+                            }
+                            else
+                            {
+                                Response.Write("<script>alert('Data insertion failed');</script>");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+                Response.Write($"Error Type: {ex.GetType()}<br/>");
+                Response.Write($"Stack Trace: {ex.StackTrace}<br/>");
+            }
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cs = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=db; Integrated Security=True;";
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    string query = "SELECT * FROM demo";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            sda.Fill(dt);
+
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+
+                            DataList1.DataSource = dt;
+                            DataList1.DataBind();
+
+                            ListView1.DataSource = dt;
+                            ListView1.DataBind();
+
+                            Repeater1.DataSource = dt;
+                            Repeater1.DataBind();
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write($"<script>alert('Database error: {sqlEx.Message}');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+                Response.Write($"Error Type: {ex.GetType()}<br/>");
+                Response.Write($"Stack Trace: {ex.StackTrace}<br/>");
+            }
+        }
+
+        // delete all
+        protected void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cs = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=db; Integrated Security=True;";
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    string query = "DELETE FROM demo";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Response.Write($"<script>alert('All records deleted successfully');</script>");
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('No records to delete');</script>");
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write($"<script>alert('Database error: {sqlEx.Message}');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+                Response.Write($"Error Type: {ex.GetType()}<br/>");
+                Response.Write($"Stack Trace: {ex.StackTrace}<br/>");
+            }
+        }
+
+        //delete by name 
+
+        protected void btnDeleteRow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string firstName = TextBox1.Text;
+                string lastName = TextBox2.Text;
+
+                if (string.IsNullOrEmpty(firstName) && string.IsNullOrEmpty(lastName))
+                {
+                    Response.Write("<script>alert('Please enter a valid first name or last name');</script>");
+                    return;
+                }
+
+                string cs = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=db; Integrated Security=True;";
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    string query;
+                    if (!string.IsNullOrEmpty(firstName))
+                    {
+                        query = "DELETE FROM demo WHERE FirstName = @firstName";
+                    }
+                    else
+                    {
+                        query = "DELETE FROM demo WHERE LastName = @lastName";
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        if (!string.IsNullOrEmpty(firstName))
+                        {
+                            cmd.Parameters.AddWithValue("@firstName", firstName);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@lastName", lastName);
+                        }
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Response.Write($"<script>alert('Record deleted successfully');</script>");
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('No record found with the specified name');</script>");
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write($"<script>alert('Database error: {sqlEx.Message}');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+                Response.Write($"Error Type: {ex.GetType()}<br/>");
+                Response.Write($"Stack Trace: {ex.StackTrace}<br/>");
+            }
+        }
+    }
+}
